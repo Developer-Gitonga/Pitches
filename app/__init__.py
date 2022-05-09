b140b84f3bde52c5851e1c3afea5c1fe7bd95cc7
Dev Git <95219841+Developer-Gitonga@users.noreply.github.com>
2022-05-09 21:05:58 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 
-
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
