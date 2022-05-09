2eebe64339722e9830c965e00af94a6b27493ccf
Dev Git <95219841+Developer-Gitonga@users.noreply.github.com>
2022-05-09 21:08:13 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
-    
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
