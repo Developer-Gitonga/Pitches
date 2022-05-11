833b32f1e36e6ab787c1d2895aa20d291f51ebaa
Dev Git <95219841+Developer-Gitonga@users.noreply.github.com>
2022-05-11 14:58:43 +0300

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length
 
 class Pitches(FlaskForm):
     title =StringField('Title',validators=[DataRequired(),Length(min=2, max=20)],render_kw={"placeholder": "Title"})
-    description = TextAreaField('Description', validators=[DataRequired(),Length(min=2, max=200)],render_kw={"placeholder": "What's the pitch about"})
+    description = TextAreaField('Description', validators=[DataRequired(),Length(min=2, max=200)],render_kw={"placeholder": "Ready to Pitch"})
     submit = SubmitField('Publish')
 
     
